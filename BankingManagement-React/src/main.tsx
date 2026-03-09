aec710451f77f7a9aacc3df3cc04af40bf3a7489
Paul <paulcode1234@gmail.com>
2026-03-09 12:02:33 +0530

    project setup completed

--- BankingManagement-React/src/main.tsx
+++ BankingManagement-React/src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/router.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
+)
+
+const root: HTMLElement | null = document.getElementById("root");
+
+if (!root) {
+  throw new Error("Root element not found");
+}
+
+ReactDOM.createRoot(root).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 )
